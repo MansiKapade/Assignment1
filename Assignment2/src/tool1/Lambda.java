71e2892844a56143069a728698e30caee39792fa
Anudip <Anudip@DESKTOP-VDMVEJN>
2023-02-08 17:56:42 +0530

    Done

---
+++ Assignment2/src/tool1/Lambda.java
@@ -0,0 +1,33 @@
+package tool1;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Lambda {
+	
+
+			public static void main(String[] args) {
+				// TODO Auto-generated method stub
+				Scanner scan=new Scanner(System.in);
+				System.out.println("enter size of the list");
+				int listnum=scan.nextInt();
+				
+				List<Integer> li=new ArrayList<Integer>();
+				
+				for(int i=0;i<listnum;i++) {
+					
+					li.add(scan.nextInt());
+				}
+				
+				System.out.println("Odd Numbers are");
+				li.forEach(s->
+				{
+					if(s%2==1) //lambda expression is used to compare each elements of the list
+					System.out.print(" "+s);
+					
+				});
+				
+			}
+
+		}
